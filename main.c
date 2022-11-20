96fff10e27c9b714f9e378d4e46efd97a4c6ab48
Cynthia <rlawldus0233@naver.com>
2022-11-20 21:38:36 +0900

    17일 수업
    17일 수업

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "copy.h"
+
+int main()
+{
+    char*str1 = "Worldcup! Final 4";
+    char* str2 = copy(str);
+    prntf("%s\n", str2);
+    return 0;
+}
